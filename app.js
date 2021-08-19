6cbbdc24580bf048ba23d448190b7ec98490ddb4
Zamikho <subecerra@ucdavis.edu>
2021-08-18 18:11:00 -0700

    began refactoring campground routes

--- app.js
+++ app.js
@@ -9,6 +9,9 @@ const ExpressError = require('./utils/ExpressError');
 const {campgroundSchema, reviewSchema} = require('./schemas.js');
 const Review = require('./models/review')
 
+const campgrounds = require('./routes/campgrounds'); 
+
+
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp', {
     useNewUrlParser : true,
@@ -30,21 +33,10 @@ app.set('view engine', 'ejs');
 
 //Needed to pass the response body during instanciation
 app.use(express.urlencoded({extended: true}));
-
 app.use(methodOverride('_method'));
+app.use('/campgrounds', campgrounds)
 
 //Middlewear function 
-const validateCampground = (req,res,next) => {
-    const {error} = campgroundSchema.validate(req.body);
-    if (error){
-        //iterate over errors and create a single string error message
-        const msg = error.details.map(el => el.message).join(',')
-        throw new ExpressError(msg, 400)
-    }else{
-        next(); 
-    }
-}
-
 const validateReview = (req,res,next) => {
     const {error} = reviewSchema.validate(req.body);
     if (error){
@@ -62,25 +54,7 @@ app.get('/', (req, res) => {
 })
 
 ///////////// REST ROUTES /////////////
-//ROUTE NAME: INDEX
-app.get('/campgrounds', catchAsync(async(req, res) => {
-    const campgrounds = await Campground.find({});
-    res.render('campgrounds/index', {campgrounds})
-}))
-
-//ROUTE NAME: NEW
-app.get('/campgrounds/new', (req, res) => {
-    res.render('campgrounds/new');
-})
-
 //ROUTE NAME: CREATE
-app.post('/campgrounds', validateCampground, catchAsync(async(req, res) => {
-    //if (!req.body.campground) throw new ExpressError("Invalid Campground Data", 400);
-    const campground = new Campground(req.body.campground);
-    await campground.save();
-    res.redirect(`/campgrounds/${campground._id}`)
-}))
-
 app.post('/campgrounds/:id/reviews', validateReview, catchAsync(async(req, res) =>{
     const campground = await Campground.findById(req.params.id);
     const review = new Review(req.body.review);
@@ -90,34 +64,7 @@ app.post('/campgrounds/:id/reviews', validateReview, catchAsync(async(req, res) 
     res.redirect(`/campgrounds/${campground._id}`);
 }))
 
-//ROUTE NAME: SHOW
-app.get('/campgrounds/:id', catchAsync(async(req, res) => {
-    const { id } = req.params;
-    const campground = await Campground.findById(id).populate('reviews');
-    res.render('campgrounds/show', {campground})
-}))
-
-//ROUTE NAME: EDIT
-app.get('/campgrounds/:id/edit', catchAsync(async(req, res) => {
-    const { id } = req.params;
-    const campground = await Campground.findById(id);
-    res.render('campgrounds/edit', {campground})
-}))
-
-//ROUTE NAME: UPDATE
-app.put('/campgrounds/:id', validateCampground, catchAsync(async(req, res) => {
-    const {id} = req.params;
-    const campground = await Campground.findByIdAndUpdate(id,{...req.body.campground})
-    res.redirect(`/campgrounds/${campground._id}`)
-}))
-
 //ROUTE NAME: DELETE
-app.delete('/campgrounds/:id', catchAsync(async(req, res) => {
-    const {id} = req.params;
-    await Campground.findByIdAndDelete(id)
-    res.redirect('/campgrounds')
-}))
-
 app.delete('/campgrounds/:id/reviews/:reviewId', catchAsync( async(req, res) => {
     const {id, reviewId} = req.params;
     //using mongo operator $pull: take anything from reviews with given 'reviewId' and take it out or 'pull' it out
